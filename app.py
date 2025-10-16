2f4a7d3d8109c398c394b8b77c519b2ff61755b3
Sylvain Bourgeois <sylvainoman@outlook.fr>
2025-10-16 10:14:58 +0200

    Add intentional vulnerability for CodeQL

--- app.py
+++ app.py
@@ -7,7 +7,13 @@ app = Flask(__name__)
 def hello():
     return "Bonjour ! Bienvenue sur notre projet de TP DevSecOps."
 
-
+@app.route('/files')
+def list_files():
+    directory = request.args.get('dir', '.')
+    # La commande 'ls' est concaténée avec une entrée utilisateur sans validation.
+    command = "ls -l " + directory
+    file_list = os.popen(command).read()
+    return f"<pre>{file_list}</pre>"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
