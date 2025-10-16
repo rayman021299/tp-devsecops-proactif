28cf26740a223f1c08b2d984f2975fcd7f5c1417
Sylvain Bourgeois <sylvainoman@outlook.fr>
2025-10-16 10:07:40 +0200

    Removed comment

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
     return "Bonjour ! Bienvenue sur notre projet de TP DevSecOps."
 
-# On ajoutera des vulnérabilités ici plus tard !
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
